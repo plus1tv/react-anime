bf470a6704d5f11ecf60356b14b058091573dff1
Kennet Postigo <kennetfpostigo@gmail.com>
2016-09-07 23:58:52 -0400

    added key prop for multichildren

--- src/Anime.js
+++ src/Anime.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import anime from 'animejs';
 
 class Anime extends React.Component {
-  
+
   constructor (props) {
     super(props);
     this.ref = [];
@@ -18,9 +18,9 @@ class Anime extends React.Component {
   addRef = (newRef) => {
     this.ref = [...this.ref, newRef];
   }
-  
+
   render () {
-  
+
     let children = [];
     if (this.props.children) {
       if (Array.isArray(this.props.children))
@@ -28,10 +28,10 @@ class Anime extends React.Component {
       else
         children = [];
     }
-  
+
     return  (
       <div>
-      {children.map((child, i) => (React.cloneElement(child, {ref: this.addRef})))}
+        {children.map((child, i) => (React.cloneElement(child, {key: i, ref: this.addRef})))}
       </div>
     );
   }
