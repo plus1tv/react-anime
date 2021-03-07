bbed1e925af7bdb815f22ccbd39c722d94920578
Alain Galvan <hi@alain.xyz>
2021-03-06 19:52:49 -0500

    Version Update, fix for refreshing animation
    In earlier versions of React Anime, if props were changed, the whole animation would restart. This change introduces the idea of completed animations. Only new elements will animated if an anime has completed.

--- src/react-anime.tsx
+++ src/react-anime.tsx
@@ -3,132 +3,144 @@ import React, { Fragment, useRef, useCallback, useEffect } from 'react';
 import animejs, { AnimeInstance } from 'animejs';
 
 export type Easing =
-  | 'easeInSine'
-  | 'easeOutSine'
-  | 'easeInOutSine'
-  | 'easeInCirc'
-  | 'easeOutCirc'
-  | 'easeInOutCirc'
-  | 'easeInElastic'
-  | 'easeOutElastic'
-  | 'easeInOutElastic'
-  | 'easeInBack'
-  | 'easeOutBack'
-  | 'easeInOutBack'
-  | 'easeInBounce'
-  | 'easeOutBounce'
-  | 'easeInOutBounce'
-  | 'easeInQuad'
-  | 'easeOutQuad'
-  | 'easeInOutQuad'
-  | 'easeInCubic'
-  | 'easeOutCubic'
-  | 'easeInOutCubic'
-  | 'easeInQuart'
-  | 'easeOutQuart'
-  | 'easeInOutQuart'
-  | 'easeInQuint'
-  | 'easeOutQuint'
-  | 'easeInOutQuint'
-  | 'easeInExpo'
-  | 'easeOutExpo'
-  | 'easeInOutExpo'
-  | 'linear'
-  | [number, number, number, number];
+    | 'easeInSine'
+    | 'easeOutSine'
+    | 'easeInOutSine'
+    | 'easeInCirc'
+    | 'easeOutCirc'
+    | 'easeInOutCirc'
+    | 'easeInElastic'
+    | 'easeOutElastic'
+    | 'easeInOutElastic'
+    | 'easeInBack'
+    | 'easeOutBack'
+    | 'easeInOutBack'
+    | 'easeInBounce'
+    | 'easeOutBounce'
+    | 'easeInOutBounce'
+    | 'easeInQuad'
+    | 'easeOutQuad'
+    | 'easeInOutQuad'
+    | 'easeInCubic'
+    | 'easeOutCubic'
+    | 'easeInOutCubic'
+    | 'easeInQuart'
+    | 'easeOutQuart'
+    | 'easeInOutQuart'
+    | 'easeInQuint'
+    | 'easeOutQuint'
+    | 'easeInOutQuint'
+    | 'easeInExpo'
+    | 'easeOutExpo'
+    | 'easeInOutExpo'
+    | 'linear'
+    | [number, number, number, number];
 
 export type AnimeValue =
-  | {
-      value: string | number;
-      delay: ((el: Element, index?: number, len?: number) => number) | number;
-      duration?:
-        | ((el: Element, index?: number, len?: number) => number)
-        | number;
-      easing?: Easing;
-    }
-  | string
-  | number
-  | ((el: Element, index?: number) => string | number);
+    | {
+          value: string | number;
+          delay: ((el: Element, index?: number, len?: number) => number) | number;
+          duration?: ((el: Element, index?: number, len?: number) => number) | number;
+          easing?: Easing;
+      }
+    | string
+    | number
+    | ((el: Element, index?: number) => string | number);
 
 export type AnimeProps = {
-  children: React.ReactNode;
-  delay?: ((el: Element, index?: number, len?: number) => number) | number;
-  duration?: ((el: Element, index?: number, len?: number) => number) | number;
-  autoplay?: boolean;
-  loop?: number | boolean;
-  direction?: 'normal' | 'reverse' | 'alternate';
-  easing?: Easing;
-  elasticity?: number;
-  round?: number | boolean;
-  svg?: boolean;
-  begin?: Function;
-  update?: Function;
-  complete?: Function;
+    children: React.ReactNode;
+    delay?: ((el: Element, index?: number, len?: number) => number) | number;
+    duration?: ((el: Element, index?: number, len?: number) => number) | number;
+    autoplay?: boolean;
+    loop?: number | boolean;
+    direction?: 'normal' | 'reverse' | 'alternate';
+    easing?: Easing;
+    elasticity?: number;
+    round?: number | boolean;
+    svg?: boolean;
+    begin?: Function;
+    update?: Function;
+    complete?: Function;
 
-  // DOM
-  value?: AnimeValue | AnimeValue[];
+    // DOM
+    value?: AnimeValue | AnimeValue[];
 
-  // Transformations
-  translateX?: AnimeValue | AnimeValue[];
-  translateY?: AnimeValue | AnimeValue[];
-  rotate?: AnimeValue | AnimeValue[];
-  scale?: AnimeValue | AnimeValue[];
+    // Transformations
+    translateX?: AnimeValue | AnimeValue[];
+    translateY?: AnimeValue | AnimeValue[];
+    rotate?: AnimeValue | AnimeValue[];
+    scale?: AnimeValue | AnimeValue[];
 
-  // CSS
-  opacity?: AnimeValue | AnimeValue[];
-  color?: AnimeValue | AnimeValue[];
-  backgroundColor?: AnimeValue | AnimeValue[];
+    // CSS
+    opacity?: AnimeValue | AnimeValue[];
+    color?: AnimeValue | AnimeValue[];
+    backgroundColor?: AnimeValue | AnimeValue[];
 
-  //SVG
-  points?: AnimeValue | AnimeValue[];
-  strokeDashoffset?: AnimeValue | AnimeValue[];
+    //SVG
+    points?: AnimeValue | AnimeValue[];
+    strokeDashoffset?: AnimeValue | AnimeValue[];
 
-  // Custom Props
-  [prop: string]: any;
+    // Custom Props
+    [prop: string]: any;
 };
 
 const PREFIX = '__anime__';
 
 export default function Anime(props: AnimeProps) {
-  const targets = useRef<any[]>([]);
-  const targetRefs = useRef<any[]>([]);
-  const anime = useRef<AnimeInstance | null>(null);
+    const targets = useRef<any[]>([]);
+    const targetRefs = useRef<any[]>([]);
+    const anime = useRef<AnimeInstance | null>(null);
+    const completed = useRef<any | null>(new Set<any>());
 
-  const createAnime = useCallback(() => {
-    if (targets.current.length > 0 && anime !== undefined)
-      animejs.remove(targets);
+    const createAnime = useCallback(
+        () => {
+            if (targets.current.length > 0 && anime !== undefined) animejs.remove(targets);
 
-    targets.current = [];
-    for (let ref of targetRefs.current) {
-      if (ref.current) targets.current.push(ref.current);
-    }
+            targets.current = [];
+            for (let ref of targetRefs.current) {
+                if (ref.current) targets.current.push(ref.current);
+            }
 
-    const animeProps: any = { ...props, targets: targets.current };
-    delete animeProps.children;
-    delete animeProps.svg;
-    anime.current = animejs(animeProps);
-  }, [props]);
+            const animeProps: any = {
+                ...props,
+                targets: targets.current,
+                complete: (ani) => {
+                    if (props.complete) {
+                        props.complete(ani);
+                    }
 
-  useEffect(() => {
-    createAnime();
-  }, [createAnime]);
+                    ani.animatables.map(a => completed.current.add(a.target));
+                }
+            };
+            delete animeProps.children;
+            delete animeProps.svg;
+            anime.current = animejs(animeProps);
+        },
+        [ props ]
+    );
 
-  const refs = targetRefs.current;
-  if (!Array.isArray(props.children)) props.children = [props.children];
-  return (
-    <Fragment>
-      {
-        // @ts-ignore
-        props.children.map((child: any, i: number) => {
-          refs.push(React.createRef());
-          // eslint-disable-next-line
-          const El = props.svg ? 'g' : 'div';
-          return (
-            <El ref={refs[refs.length - 1]} key={`${PREFIX}${i}`}>
-              {child}
-            </El>
-          );
-        })
-      }
-    </Fragment>
-  );
+    useEffect(
+        () => {
+            createAnime();
+        },
+        [ createAnime ]
+    );
+
+    const refs = targetRefs.current;
+    if (!Array.isArray(props.children)) props.children = [ props.children ];
+    return (
+        <Fragment>
+            {// @ts-ignore
+            props.children.map((child: any, i: number) => {
+                refs.push(React.createRef());
+                // eslint-disable-next-line
+                const El = props.svg ? 'g' : 'div';
+                return (
+                    <El ref={refs[refs.length - 1]} key={`${PREFIX}${i}`}>
+                        {child}
+                    </El>
+                );
+            })}
+        </Fragment>
+    );
 }
